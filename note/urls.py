f2eb7e78ccc78a0afcef80b335646fa739feb0a3
avadh <rakholiyabalo@gmail.com>
2024-01-23 22:10:03 +0530

    Create Notes

--- note/urls.py
+++ note/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
 
     path('notes/',views.list,name='notelist' ),
-    path('notes/<id>',views.detail )
+
+    path('notes/new', views.create_note, name='create_note'),
+    path('notes/<id>', views.detail, name='detail'),
 
 
 ]
--- note/views.py
+++ note/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Notes
 # Create your views here.
+from .forms import NotesForm
+
 def  list(request):
     all_notes=Notes.objects.all()
     return render(request,'note_list.html',{'notes':all_notes})
@@ -8,4 +10,15 @@ def  list(request):
 def detail(request,id):
     note=Notes.objects.get(pk=id)
     return render(request,'note_detail.html',{'note':note})
-    
+
+def create_note(request):
+    if request.method == 'POST':
+        form = NotesForm(request.POST)
+        if form.is_valid():
+            note = form.save()
+            return redirect('notelist')
+    else:
+        form = NotesForm()
+
+    return render(request, 'note_form.html', {'form': form})
+

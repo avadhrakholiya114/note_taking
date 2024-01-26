5795c4159198d419a29607df193bed4b39c51d7e
avadh <rakholiyabalo@gmail.com>
2024-01-26 15:54:20 +0530

    .

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
 echo "BUILD START"
-/usr/bin/python -m ensurepip
-/usr/bin/python -m pip install --upgrade pip
-/usr/bin/python -m pip install -r requirements.txt
-/usr/bin/python manage.py collectstatic --noinput --clear
+python3.0 -m pip install -r requirements.txt
+python3.0 manage.py collectstatic --noinput --clear
 echo "BUILD END"
